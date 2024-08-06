3d3dcc93077bc743f99a87ca17fc14ea7df4cbf3
TopAcademy2022 <popov_r_top@outlook.com>
2024-08-06 20:18:18 +0300

    Goood

--- ai-picture.h
+++ ai-picture.h
@@ -3,10 +3,19 @@
 
 #include <QObject>
 #include <QDialog>
+#include <QNetworkAccessManager>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QNetworkReply>
 
 class AIPicture : public QObject
 {
     Q_OBJECT
+private:
+    QNetworkAccessManager* _networkManager;
+
+    void GetGeneratedPictureUrl(QString textForGeneration);
+
 public:
     explicit AIPicture(QWidget* mainWindow, QObject *parent = nullptr);
 
